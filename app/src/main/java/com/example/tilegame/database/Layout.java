3d2a90b4d5ae7d01066e178a55ebed86f1e234e3
Patrick Gorman <gormanpe@vcu.edu>
2019-04-04 03:50:49 -0400

    Added initial database storage object files

---
+++ app/src/main/java/com/example/tilegame/database/Layout.java
@@ -0,0 +1,36 @@
+package com.example.tilegame.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import com.example.tilegame.tileLayout.TileLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(tableName = "layout_table")
+public class Layout {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "name")
+    String name;
+
+    @NonNull
+    @ColumnInfo(name = "type")
+    private List<Tile> tiles;
+
+    public Layout(@NonNull TileLayout layout, @NonNull String name) {
+
+        this.name = name;
+        this.tiles = new ArrayList<Tile>();
+        for(int x=0; x<9; x++){
+            for(int y=0; y<13; y++){
+                Tile tile = new Tile(x, y, layout.getTile(x, y).name);
+                tiles.add(tile);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tilegame/database/Tile.java
@@ -0,0 +1,41 @@
+package com.example.tilegame.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "tile_table")
+public class Tile {
+
+
+    @ColumnInfo(name = "x")
+    public int x;
+
+    @ColumnInfo(name = "y")
+    public int y;
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "coordinate")
+    String coord;
+
+    @NonNull
+    @ColumnInfo(name = "type")
+    private String type;
+
+    public Tile(int x, int y, @NonNull String type) {
+
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.coord = "("+x+","+")";
+    }
+
+    @NonNull
+    public String getType() {
+
+        return this.type;
+    }
+
+}
